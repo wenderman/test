f4cc29be5323451d22b6d903cf6c97d07c71ceda
wenderman <55359095+wenderman@users.noreply.github.com>
2020-12-11 21:24:43 +0300

    Delete sss.ps1

--- sss.ps1
+++
@@ -1,8 +0,0 @@
-$word = New-Object -ComObject Word.Application
-$word.Visible = $True
-$doc = $word.Documents.Add()
-$Selection = $word.Selection
-$Selection.TypeParagraph()
-$Selection.Font.Name = "Times New Roman"
-$Selection.Font.Size = 18
-$Selection.TypeText("Hello world!")
